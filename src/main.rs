3fe301822ce7d4ea405ce8adf6842c2c1c635a72
Liam Storgaard <46511698+liamsto@users.noreply.github.com>
2024-12-05 15:39:16 -0800

    basic VGA logic following blog-os tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga = 0xb8000 as *mut u8;
+    
+    for(i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga.offset(i as isize * 2) = byte;
+            *vga.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
