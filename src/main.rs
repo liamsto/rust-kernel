65dd3ddc2bf2c32bf57caf59799577a36abb6547
liamsto <liamsto@outlook.com>
2024-12-05 16:38:23 -0800

    successfully created bootimage

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 #![no_std]
 #![no_main]
 
+use core::panic::PanicInfo;
+
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga = 0xb8000 as *mut u8;
-    
-    for(i, &byte) in HELLO.iter().enumerate() {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
-            *vga.offset(i as isize * 2) = byte;
-            *vga.offset(i as isize * 2 + 1) = 0xb;
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
 
@@ -18,6 +20,6 @@ pub extern "C" fn _start() -> ! {
 }
 
 #[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
